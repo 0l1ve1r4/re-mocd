bd988668556b28a8e800527a8f579ec4e6bb1fcf
0l1ve1r4 <gssantoz2012@gmail.com>
2024-12-08 12:20:46 -0300

    Added modularity + compatibility with egui_graphs

--- src/graph.rs
+++ src/graph.rs
@@ -1,28 +1,87 @@
-use petgraph::graph::UnGraph;
+use petgraph::stable_graph::{StableGraph, NodeIndex};
+use std::collections::HashMap;
 
-// Define a wrapper struct
-pub struct Graph<N, E> {
-    graph: UnGraph<N, E>,
+/// Representation of a community partition
+#[derive(Debug, Clone)]
+pub struct Partition {
+    pub communities: HashMap<usize, Vec<NodeIndex>>, // community_id -> nodes
 }
 
-impl<N, E> Graph<N, E> {
-    pub fn new() -> Self {
-        Graph {
-            graph: UnGraph::new_undirected(),
+pub fn calculate_objectives(graph: &StableGraph<(), ()>, partition: &Partition) -> f64 {
+    let total_edges = graph.edge_count();
+    let mut intra = 0.0;
+    let mut inter = 0.0;
+
+    for community in partition.communities.values() {
+        let mut intra_edges = 0;
+        let mut total_degree = 0;
+
+        for &node in community {
+            total_degree += graph.neighbors(node).count();
+            for neighbor in graph.neighbors(node) {
+                if community.contains(&neighbor) {
+                    intra_edges += 1;
+                }
+            }
         }
+
+        intra += intra_edges as f64 / (2.0 * total_edges as f64);
+        inter += (total_degree as f64 / (2.0 * total_edges as f64)).powi(2);
     }
 
-    pub fn print(&self) {
-        println!("{:?}", &self);
-    }
+    intra /= 2.0;
 
-    pub fn generate_example_graph(&mut self) {
-        let g = UnGraph::<i32, ()>::from_edges(&[
-            (1, 2), (2, 3), (3, 4),
-            (1, 4)
-        ]);
+    println!("Modularity: {}, Intra: {}, Inter: {}", 1.0 - intra - inter, intra, inter);
 
-        // Set the graph inside the wrapper
-        self.graph = g;
-    }
+    1.0 - intra - inter
+}
+
+// Example graph where intra-objective = 1 (no intra edges)
+pub fn high_intra_graph() -> StableGraph<(), ()> {
+    let mut graph_intra = StableGraph::<(), ()>::new();
+    let n0 = graph_intra.add_node(());
+    let n1 = graph_intra.add_node(());
+    let n2 = graph_intra.add_node(());
+    let n3 = graph_intra.add_node(());
+
+    graph_intra.add_edge(n0, n1, ());
+    graph_intra.add_edge(n2, n3, ());
+
+    let partition_intra = Partition {
+        communities: HashMap::from([(0, vec![n0, n2]), (1, vec![n1, n3])]),
+    };
+
+    let modularity = calculate_objectives(&graph_intra, &partition_intra);
+
+    println!("Graph with intra-objective = 1");
+    println!("Modularity: {:.4}", modularity);
+
+    graph_intra
+}
+
+pub fn high_inter_graph() -> StableGraph<(), ()> {
+    // Example graph where inter-objective = 1 (perfectly balanced degrees)
+    let mut graph_inter = StableGraph::<(), ()>::new();
+    let n0 = graph_inter.add_node(());
+    let n1 = graph_inter.add_node(());
+    let n2 = graph_inter.add_node(());
+    let n3 = graph_inter.add_node(());
+
+    graph_inter.add_edge(n0, n1, ());
+    graph_inter.add_edge(n0, n2, ());
+    graph_inter.add_edge(n0, n3, ());
+    graph_inter.add_edge(n1, n2, ());
+    graph_inter.add_edge(n1, n3, ());
+    graph_inter.add_edge(n2, n3, ());
+
+    let partition_inter = Partition {
+        communities: HashMap::from([(0, vec![n0, n1, n2, n3])]),
+    };
+
+    let modularity = calculate_objectives(&graph_inter, &partition_inter);
+
+    println!("Graph with inter-objective = 1");
+    println!("Modularity: {:.4}", modularity);
+
+    graph_inter
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,64 @@
-use petgraph::graph::UnGraph;
+use eframe::{run_native, App, CreationContext};
+use egui::Context;
+use egui_graphs::{DefaultGraphView, Graph, SettingsInteraction, SettingsStyle};
 
+mod  graph;
+use graph::{
+    high_inter_graph,
+    high_intra_graph
+};
+
+pub struct InteractiveApp {
+    g: Graph,
+}
+
+impl InteractiveApp {
+    fn new(_: &CreationContext<'_>) -> Self {
+        let g: Graph = generate_graph();
+        Self { g }
+    }
+}
+
+impl App for InteractiveApp {
+    fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let interaction_settings = &SettingsInteraction::new()
+                .with_dragging_enabled(true)
+                .with_node_clicking_enabled(true)
+                .with_node_selection_enabled(true)
+                .with_node_selection_multi_enabled(false)
+                .with_edge_clicking_enabled(true)
+                .with_edge_selection_enabled(true)
+                .with_edge_selection_multi_enabled(false)
+                .with_edge_selection_enabled(true);
+                
+            let style_settings = &SettingsStyle::new().with_labels_always(false);
+            ui.add(
+                &mut DefaultGraphView::new(&mut self.g)
+                    .with_styles(style_settings)
+                    .with_interactions(interaction_settings),
+            );
+        });
+    }
+}
+
+fn generate_graph() -> Graph {
+    if true {
+        return Graph::from(&high_inter_graph())
+    }
+
+    else {
+        return Graph::from(&high_intra_graph())
+    }
+
+}
 
 fn main() {
-// Create an undirected graph with `i32` nodes and edges with `()` associated data.
-let g = UnGraph::<i32, ()>::from_edges(&[
-    (1, 2), (2, 3), (3, 4),
-    (1, 4)]);
-
-// Output the tree to `graphviz` `DOT` format
-println!("{:?}", &g);
-// graph {
-//     0 [label="\"0\""]
-//     1 [label="\"0\""]
-//     2 [label="\"0\""]
-//     3 [label="\"0\""]
-//     1 -- 2
-//     3 -- 4
-//     2 -- 3
-// }
+    let native_options = eframe::NativeOptions::default();
+    run_native(
+        "egui_graphs_interactive_demo",
+        native_options,
+        Box::new(|cc| Ok(Box::new(InteractiveApp::new(cc)))),
+    )
+    .unwrap();
 }
