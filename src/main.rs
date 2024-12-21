3c7cdf9f3a5d061b4c9e6b98876da676af6a53ad
0l1ve1r4 <gssantoz2012@gmail.com>
2024-12-21 14:43:03 -0300

    feat [main.rs]: graph file and debug mode in command line args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use petgraph::Undirected;
 use petgraph::graph::{Graph, UnGraph};
+use petgraph::Undirected;
 
-use std::fs::File;
+use std::env;
+use std::fs::{File, exists};
 use std::path::Path;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use std::io::{self, BufRead, Write};
 
 mod algorithm;
@@ -13,25 +14,22 @@ const POPULATION_SIZE: usize = 100;
 
 fn read_graph(file_path: &str) -> UnGraph<(), ()> {
     let mut graph: Graph<(), (), Undirected> = Graph::new_undirected();
-
     let mut node_indices = std::collections::HashMap::new();
 
     // Open the file and iterate over its lines
     if let Ok(lines) = read_lines(file_path) {
         for line in lines {
             if let Ok(entry) = line {
-                // Parse the line into source and target
                 let parts: Vec<&str> = entry.split(',').collect();
                 if parts.len() >= 2 {
-                    let source = parts[0].parse::<usize>().unwrap();
-                    let target = parts[1].parse::<usize>().unwrap();
-
-                    // Add nodes to the graph if they don't exist
-                    let src_index = *node_indices.entry(source).or_insert_with(|| graph.add_node(()));
-                    let tgt_index = *node_indices.entry(target).or_insert_with(|| graph.add_node(()));
-
-                    // Add an edge between the nodes
-                    graph.add_edge(src_index, tgt_index, ());
+                    // Safely parse node indices
+                    if let (Ok(source), Ok(target)) = (parts[0].parse::<usize>(), parts[1].parse::<usize>()) {
+                        // Add nodes to the graph if they don't exist
+                        let src_index = *node_indices.entry(source).or_insert_with(|| graph.add_node(()));
+                        let tgt_index = *node_indices.entry(target).or_insert_with(|| graph.add_node(()));
+                        // Add an edge between the nodes
+                        graph.add_edge(src_index, tgt_index, ());
+                    }
                 }
             }
         }
@@ -39,7 +37,6 @@ fn read_graph(file_path: &str) -> UnGraph<(), ()> {
     graph
 }
 
-// Helper function to read lines from a file
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -48,48 +45,83 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn main() {
-    let start: Instant = std::time::Instant::now();
+fn debug(reading_time: Duration, algorithm_time: Duration) {
+    println!("[Debug Mode]:");
+    println!("- Reading time: {:?}", reading_time);
+    println!("- Algorithm time: {:?}", algorithm_time);
 
-    let file_path = "/home/ol1ve1r4/Desktop/mocd/src/graphs/artificials/karate.edgelist";
-    let mut data: Vec<String> = Vec::new();
-    let graph: Graph<(), (), Undirected> = 
-    read_graph(&file_path);
-        
-    println!("Reading elapsed: {:.2?}", start.elapsed());
+}
 
-    let start: Instant = std::time::Instant::now();
-
-    let (
-        _,
-        _,
-        _,
-        _,
-        best_history,
-        avg_history,
-    ) = algorithm::genetic_algorithm(&graph, NUM_GENERATIONS, POPULATION_SIZE);
-
-    let elapsed = start.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
-
-        // Save in DataFrame
+fn save_data(best_history: Vec<f64>, avg_history: Vec<f64>) {
+    let mut data: Vec<String> = Vec::with_capacity(NUM_GENERATIONS);
     for generation in 0..NUM_GENERATIONS {
-        data.push(format!(
-            "{},{},{}",
-            generation,
-            best_history[generation],
-            avg_history[generation],
-        ));
+     data.push(format!(
+         "{},{},{}",
+         generation,
+         best_history[generation],
+         avg_history[generation]
+     ));
+ }
+ 
+    let mut file = 
+        File::create("generations_data.csv")
+        .expect("Unable to create file");
+    
+    writeln!(file, "generation,best_fitness,avg").unwrap();
+    for line in data {
+         writeln!(file, "{}", line).unwrap();
     }
     
-    let mut file = File::create("generations_data.csv").expect("Unable to create file");
-    writeln!(
-        file,
-        "generation,best_fitness,avg"
-    )
-    .unwrap();
-    for line in data {
-        writeln!(file, "{}", line).unwrap();
-    }
     println!("Data saved to generations_data.csv");
+}
+
+fn main() -> () {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: <program> <file_path> [ -d for debug ]");
+        println!("Example: ./my_program ../graphs/artificial/karate.edgelist -d");
+        return;
+    }
+
+    let file_path: &str = &args[1];
+    if !exists(file_path).unwrap_or_else(|err| {
+        eprintln!("Failed to check file existence: {}", err);
+        false
+    }) {
+        println!("Graph .edgelist file not found.");
+        return;
+    }
+
+
+
+    let debug_mode: bool = args.len() > 2 && &args[2] == "-d";
+    if debug_mode {
+        println!("Debug mode: {}", debug_mode);
+        println!("File found, proceed with reading...");
+    }
+
+
+    let start: Instant = Instant::now();
+    let graph: Graph<(), (), Undirected> = read_graph(file_path);
+    let reading_time: Duration = start.elapsed();
+
+    let start: Instant = Instant::now();
+    let (_, 
+        _, 
+        _, 
+        _,
+        best_history,
+        avg_history
+    ) = algorithm::genetic_algorithm(&graph, NUM_GENERATIONS, POPULATION_SIZE,);
+
+    let algorithm_time: Duration = start.elapsed();
+
+    if debug_mode {
+        debug(reading_time, algorithm_time);
+    }
+
+    save_data(best_history, avg_history);
+
+   
 }
