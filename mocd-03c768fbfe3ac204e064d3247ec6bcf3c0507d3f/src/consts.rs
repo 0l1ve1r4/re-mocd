e3ccb73270f30afea7cd79673bfba4cc2ccf4578
0l1ve1r4 <gssantoz2012@gmail.com>
2024-12-23 16:09:26 -0300

    add: .old

--- mocd-03c768fbfe3ac204e064d3247ec6bcf3c0507d3f/src/consts.rs
+++
@@ -1,6 +0,0 @@
-// This Source Code Form is subject to the terms of The GNU General Public License v3.0
-// Copyright 2024 - Guilherme Santos. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
-
-pub const NUM_GENERATIONS: usize = 120;
-pub const POPULATION_SIZE: usize = 20;
